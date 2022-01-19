91642e7c44c90877bb41e8590d0ddf2f36127ad3
Karl Stolley <karl.stolley@gmail.com>
2022-01-19 15:43:19 -0600

    Add timed async callback

--- timeout.js
+++ timeout.js
@@ -1,2 +1,11 @@
-console.log('Hello! I am the first statement');
-console.log('Hello! I am the second statement');
+const start = Date.now();
+
+setTimeout(function() {
+  const startCallback = Date.now();
+  const delay = startCallback - start;
+  console.log('Hello, I am the first async statement');
+  console.log(`I ran after ${delay} milliseconds`);
+}, 1000);
+
+console.log('Hello! I am the first sync statement');
+console.log('Hello! I am the second sync statement');
