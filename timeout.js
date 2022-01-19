7adef1f1ff2b6810504cf5a75c64e53f0380270c
Karl Stolley <karl.stolley@gmail.com>
2022-01-19 15:50:53 -0600

    Use a named callback with setTimeout

--- timeout.js
+++ timeout.js
@@ -1,11 +1,13 @@
 const start = Date.now();
 
-setTimeout(function() {
+function reportDelay() {
   const startCallback = Date.now();
   const delay = startCallback - start;
   console.log('Hello, I am the first async statement');
   console.log(`I ran after ${delay} milliseconds`);
-}, 1000);
+}
+
+setTimeout(reportDelay, 1000);
 
 console.log('Hello! I am the first sync statement');
 console.log('Hello! I am the second sync statement');
