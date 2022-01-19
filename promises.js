069a5e936fd3d6a5b4b70404010af0016acab39d
Karl Stolley <karl.stolley@gmail.com>
2022-01-19 16:21:42 -0600

    Add Promises and async/await syntax

--- promises.js
+++ promises.js
@@ -8,5 +8,21 @@ async function waitForIt(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+async function waitForData() {
+  return new Promise(resolve => resolve(`Tasty, tasty data`));
+}
+
+waitForData()
+  .then(data => console.log(`Value of my thenned data: ${data}`));
+
+(async function() {
+  const myData = await waitForData();
+  console.log(`Value of myData: ${myData}`)
+})();
+
+(function() {
+  console.log('Run me right away');
+})();
+
 waitForIt(1000)
   .then(myTurn);
