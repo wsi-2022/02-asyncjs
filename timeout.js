b105531f4286553c6d211a2c31d0152c5c53fbb0
Karl Stolley <karl.stolley@gmail.com>
2022-01-19 15:51:07 -0600

    Illustrate browser and event-loop things

--- timeout.js
+++ timeout.js
@@ -11,3 +11,14 @@ setTimeout(reportDelay, 1000);
 
 console.log('Hello! I am the first sync statement');
 console.log('Hello! I am the second sync statement');
+
+/*
+// BROWSER CODE! For explanation only!
+var button = document.querySelector('button');
+button.addEventListener('click', doSomethingWithButton);
+
+// Imagine a world without events and event loop:
+while (true) {
+  if (hearClick) break; // not gonna work
+}
+*/
